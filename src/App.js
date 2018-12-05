495c6d2614cb86883a0afe1eccea92bce74cd315
rayqi <qi.raymond@gmail.com>
2018-12-05 13:22:48 -0500

    add(App) installed materialize and created general outline on main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "",
+      collection: []
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <div className="header">
+            <h1>giphy world</h1>
+            <h3>the best gifs in giphy</h3>
+          </div>
+        </div>
+        <div className="body container">
+          <div class="row">
+            <form class="col s12">
+              <div class="row">
+                <div class="input-field col s12">
+                  <textarea id="textarea1" class="materialize-textarea"></textarea>
+                  <label for="textarea1">search giphy</label>
+                  <button class="btn waves-effect waves-light" type="submit" name="action">EXPLORE
+                    <i class="material-icons right">send</i>
+                  </button>
+                </div>
+
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
