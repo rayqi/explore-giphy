c45bffc79863233ac1bec6a8098daf7613cc4041
rayqi <qi.raymond@gmail.com>
2018-12-05 13:28:39 -0500

    add(server) added server folder and installed dependencies. set up b/e server

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 5000;
+
+
+
+app.use(morgan('dev'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
+
+app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+app.listen(port, () => {
+    console.log('server is running nicely at 5000')
+})
+
+module.exports = app;
