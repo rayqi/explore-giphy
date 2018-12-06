7db0bfa4045972757f41337bb32d38fea5e62833
rayqi <qi.raymond@gmail.com>
2018-12-06 16:21:27 -0500

    remove unneeded routes and update scripts

--- server/index.js
+++ server/index.js
@@ -5,28 +5,10 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 5000;
 
-
-
 app.use(morgan('dev'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-
-app.use('/api', require('./route'))
-
-
-app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-})
-
-
-
-app.use((err, req, res, next) => {
-    console.error(err)
-    console.error(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error.')
-})
 
 // if (process.env.NODE_ENV === 'production') {
 //     // Serve any static files
@@ -38,12 +20,19 @@ app.use((err, req, res, next) => {
 // }
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
+
+app.use('/api', require('./route'))
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
+app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
 
 app.listen(port, () => {
     console.log('server is running nicely at 5000')
--- server/route.js
+++ server/route.js
@@ -7,14 +7,14 @@ const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 router.get('/:content', (req, res, next) => {
     console.log('hitting here')
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${req.params.content}&api_key=${API_KEY}`)
+    return axios.get(`http://api.giphy.com/v1/gifs/search?q=${req.params.content}&api_key=${API_KEY}`)
         .then(res => res.data)
         .then(results => res.send(results))
 })
 
 router.get('/trending', (req, res, next) => {
     console.log('hitting here')
-    axios.get(`http://api.giphy.com/v1/gifs/trending&api_key=${API_KEY}`)
+    return axios.get(`http://api.giphy.com/v1/gifs/trending&api_key=${API_KEY}`)
         .then(res => res.data)
         .then(results => res.send(results))
 })
