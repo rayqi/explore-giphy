084868dbdde7cb61c98f7d846d8dcbf6f0c81a3b
rayqi <qi.raymond@gmail.com>
2018-12-06 17:06:28 -0500

    update static routes

--- server/index.js
+++ server/index.js
@@ -20,12 +20,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // }
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.use('/api', require('./route'))
 
 app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
