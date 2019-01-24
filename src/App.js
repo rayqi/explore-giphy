c2d3ff4948199831e8e107a3a077672f4dbff3cf
rayqi <qi.raymond@gmail.com>
2019-01-23 23:45:49 -0500

    refactor App component to contain Header,Search,Trending components

--- src/App.js
+++ src/App.js
@@ -2,35 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 import Results from './Results'
+import Header from './Header';
+import Trending from './Trending';
+import Search from './Search';
 
-let API_KEY = "tXuAFO9PLLeRDzCzFORGmlDajArwFF5a"
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: "",
+      // name: "",
       collection: [],
-      ratings: [],
       searched: false,
-      sorted: false
+      // sorted: false
     }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    // this.handleChange = this.handleChange.bind(this)
+    // this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleChange = (event) => {
-    this.setState({ name: event.target.value })
-  }
+  // handleChange = (event) => {
+  //   this.setState({ name: event.target.value })
+  // }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.getData(this.state.name)
-    event.target.reset()
-  }
+  // handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   this.getData(this.state.name)
+  //   event.target.reset()
+  // }
 
-  handleClick = () => {
-    this.setState({ sorted: !this.state.sorted })
-  }
+  // handleClick = () => {
+  //   this.setState({ sorted: !this.state.sorted })
+  // }
 
   getData = (content) => {
     return axios.get(`/api/${content}`)
@@ -44,19 +45,25 @@ class App extends Component {
       .then(results => this.setState({ collection: results.data }))
   }
 
+  getSearching = () => {
+    this.setState({ searched: true })
+  }
+
+
+
 
   //On each filter handler, we are adding the specific value (rating) to the array.
   //The array can hold several ratings and will filter the content accordingly.
-  updateRatings = (event) => {
-    console.log('event.target.value', event.target.value, this.state.ratings)
-    if (!this.state.ratings.includes(event.target.value)) {
-      this.setState({ ratings: [...this.state.ratings, event.target.value] })
-    } else {
-      this.setState({
-        ratings: this.state.ratings.filter((elem) => elem !== event.target.value)
-      })
-    }
-  }
+  // updateRatings = (event) => {
+  //   console.log('event.target.value', event.target.value, this.state.ratings)
+  //   if (!this.state.ratings.includes(event.target.value)) {
+  //     this.setState({ ratings: [...this.state.ratings, event.target.value] })
+  //   } else {
+  //     this.setState({
+  //       ratings: this.state.ratings.filter((elem) => elem !== event.target.value)
+  //     })
+  //   }
+  // }
 
 
   //Once the landing page mounts, we will make a call to trending gifs under the search.
@@ -66,75 +73,14 @@ class App extends Component {
 
   //Passed down results to Results Component
   render() {
-    console.log('state', this.state)
+    console.log('state in App.js', this.state)
     return (
       <div className="App">
         <div className="landing-page container">
-          <div className="header">
-            <h1 className="main-title">GAY WORLD</h1>
-            <h4 className="subtitle">the best gifs in giphy</h4>
-          </div>
-          <div className="body container ">
-            <div class="row ">
-              <form class="search-bar col s12" onSubmit={this.handleSubmit}>
-                <div class="row">
-                  <div class="input-field col s12">
-                    <textarea id="textarea1" class="materialize-textarea" onChange={this.handleChange}></textarea>
-                    <label for="textarea1">search giphy</label>
-                    <button class="btn waves-effect waves-light" type="submit" name="action">EXPLORE
-                    <i class="material-icons right">send</i>
-                    </button>
-                  </div>
-
-                </div>
-              </form>
-            </div>
-          </div>
+          <Header />
+          <Search getData={this.getData} getSearching={this.getSearching} updateRatings={this.updateRatings} />
         </div>
-        {this.state.searched === false ? <div></div> : <div className="results container ">
-          <div className="custom-row">
-            <div className="sort-button col s2">
-              <a class="waves-effect waves-light btn-small" onClick={this.handleClick}>Most Recent</a>
-
-            </div>
-            <div className="filters-checklist">
-              <form className="filter-list flex-row" action="#">
-                <p>
-                  <label className="filter-label">
-                    <input type="checkbox" name="rating" value="PG-13" onChange={this.updateRatings} />
-                    <span>PG-13</span>
-                  </label>
-                </p>
-                <p>
-                  <label className="filter-label">
-                    <input type="checkbox" name="rating" value="PG" onChange={this.updateRatings} />
-                    <span>PG</span>
-                  </label>
-                </p>
-                <p>
-                  <label className="filter-label">
-                    <input type="checkbox" name="rating" value="Y" onChange={this.updateRatings} />
-                    <span>Y</span>
-                  </label>
-                </p>
-                <p>
-                  <label className="filter-label">
-                    <input type="checkbox" name="rating" value="G" onChange={this.updateRatings} />
-                    <span>G</span>
-                  </label>
-                </p>
-                <p>
-                  <label className="filter-label">
-                    <input type="checkbox" name="rating" value="R" onChange={this.updateRatings} />
-                    <span>R</span>
-                  </label>
-                </p>
-              </form>
-            </div>
-          </div>
-        </div>
-        }
-        <Results collection={this.state.collection} ratings={this.state.ratings} searched={this.state.searched} sorted={this.state.sorted} />
+        {!this.state.searched ? <Trending trending={this.state.collection} /> : <Results collection={this.state.collection} />}
       </div >
     );
   }
--- src/Results.js
+++ src/Results.js
@@ -5,12 +5,31 @@ import Gallery from 'react-photo-gallery'
 class Results extends Component {
     constructor(props) {
         super(props)
+        this.state = { ratings: [], sorted: "false" }
+
     }
 
+    updateRatings = (event) => {
+        if (!this.state.ratings.includes(event.target.value)) {
+            this.setState({ ratings: [...this.state.ratings, event.target.value] })
+        } else {
+            this.setState({
+                ratings: this.state.ratings.filter((elem) => elem !== event.target.value)
+            })
+        }
+    }
+
+    handleClick = () => {
+        this.setState({ sorted: !this.state.sorted })
+    }
     render() {
         let collection = this.props.collection
-        let ratings = this.props.ratings
-        let sorted = this.props.sorted
+        let ratings = this.state.ratings
+        let sorted = this.state.sorted
+        console.log('props', this.props)
+        console.log('ratings', this.state.ratings)
+
+
 
 
         //Set filtered array to be collection of images if ratings are selected with filters accordingly. 
@@ -18,10 +37,11 @@ class Results extends Component {
         let filtered = [];
         collection.length > 0 && ratings.length > 0 ? filtered = collection.filter(gif => ratings.includes(gif.rating.toUpperCase())) : filtered = collection;
 
-
+        console.log('filtered', filtered)
         //sortedList is defaulted as the filtered array based on previous logic. If sorted is toggled on App component,
         //then sort based on import_datetime. 
-        let sortedList;
+        let sortedList = filtered;
+        console.log('...', sortedList)
         if (sorted === false) {
             sortedList = filtered
         } else {
@@ -47,9 +67,53 @@ class Results extends Component {
         })
 
         return (
-            <article className="container ">
-                {gifGallery.length > 0 ? <Gallery photos={gifGallery} /> : <Default />}
-            </article>
+            <div>
+                <div className="results container ">
+                    <div className="custom-row">
+                        <div className="sort-button col s2">
+                            <a class="waves-effect waves-light btn-small" onClick={this.handleClick}>Most Recent</a>
+
+                        </div>
+                        <div className="filters-checklist">
+                            <form className="filter-list flex-row" action="#">
+                                <p>
+                                    <label className="filter-label">
+                                        <input type="checkbox" name="rating" value="PG-13" onChange={this.updateRatings} />
+                                        <span>PG-13</span>
+                                    </label>
+                                </p>
+                                <p>
+                                    <label className="filter-label">
+                                        <input type="checkbox" name="rating" value="PG" onChange={this.updateRatings} />
+                                        <span>PG</span>
+                                    </label>
+                                </p>
+                                <p>
+                                    <label className="filter-label">
+                                        <input type="checkbox" name="rating" value="Y" onChange={this.updateRatings} />
+                                        <span>Y</span>
+                                    </label>
+                                </p>
+                                <p>
+                                    <label className="filter-label">
+                                        <input type="checkbox" name="rating" value="G" onChange={this.updateRatings} />
+                                        <span>G</span>
+                                    </label>
+                                </p>
+                                <p>
+                                    <label className="filter-label">
+                                        <input type="checkbox" name="rating" value="R" onChange={this.updateRatings} />
+                                        <span>R</span>
+                                    </label>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <article className="container ">
+                    {gifGallery.length > 0 ? <Gallery photos={gifGallery} /> : <Default />}
+                </article>
+            </div>
         )
     }
 }
